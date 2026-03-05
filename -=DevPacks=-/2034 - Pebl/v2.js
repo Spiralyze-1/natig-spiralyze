c9a39378d5e07c4a2fb7fd7cebde9cbefa59ce1c
Natig Agharzayev <natig@spiralyze.com>
2026-03-05 17:59:56 +0200

    feat: 2034 (devpack), 4002

---
+++ -=DevPacks=-/2034 - Pebl/v2.js
@@ -0,0 +1,77 @@
+document.querySelector("#block--unique-id-25661 .component-content").innerHTML = `
+    <div class="spz-hero-content">
+      <div class="spz-hero-content-inner">
+        <strong>Build global teams quickly and easily</strong>
+        <p>Pebl's AI-powered EOR platform simplifies building and managing teams in 185+ countries, enabling fast, seamless growth across borders.</p>
+        <div class="spz-btn-list">
+          <a target="_blank" class="btn btn-pacific spz2034_v2" href="https://signup.hellopebl.com/self-signup" rel="noopener">Request a quote</a>
+          <a target="_blank" class="nav-link-button-watch-demo btn btn-ghost-dark spz2034_v2" href="https://hellopebl.com/demo/" rel="noopener">Watch demo</a>
+        </div>
+      </div>
+    </div>
+    <div class="spz-hero-logos spz-hero-logos-colored">
+      <div class="spz-hero-wrapper">
+        <strong>Trusted by global leaders</strong>
+        <div class="spz-logo-track">
+            <ul class="spz-logo-list">
+              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1769096875/pebl/2034/logo_01.svg" alt="LastPass" width="120" height="40"/></li>
+              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1769096875/pebl/2034/logo_02.svg" alt="crunchbase" width="120" height="40"/></li>
+              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1769096875/pebl/2034/logo_03.svg" alt="attentive" width="120" height="40"/></li>
+              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1769096875/pebl/2034/logo_04.svg" alt="Materialize" width="120" height="40"/></li>
+              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1769096875/pebl/2034/logo_05.svg" alt="consensys" width="120" height="40"/></li>
+              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1769703805/pebl/2034/logo_22.svg" alt="Penguin Random House" width="120" height="40"/></li>
+              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1769160902/pebl/2034/logo_07.svg" alt="anuvu" width="120" height="40"/></li>
+              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1769160902/pebl/2034/logo_08.svg" alt="Linksys" width="120" height="40"/></li>
+              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1769160903/pebl/2034/logo_09.svg" alt="Glaukos" width="120" height="40"/></li>
+              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1769160903/pebl/2034/logo_10.svg" alt="Paige" width="120" height="40"/></li>
+            </ul>
+            <ul aria-hidden class="spz-logo-list">
+              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1769096875/pebl/2034/logo_01.svg" alt="LastPass" width="120" height="40"/></li>
+              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1769096875/pebl/2034/logo_02.svg" alt="crunchbase" width="120" height="40"/></li>
+              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1769096875/pebl/2034/logo_03.svg" alt="attentive" width="120" height="40"/></li>
+              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1769096875/pebl/2034/logo_04.svg" alt="Materialize" width="120" height="40"/></li>
+              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1769096875/pebl/2034/logo_05.svg" alt="consensys" width="120" height="40"/></li>
+              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1769703805/pebl/2034/logo_22.svg" alt="Penguin Random House" width="120" height="40"/></li>
+              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1769160902/pebl/2034/logo_07.svg" alt="anuvu" width="120" height="40"/></li>
+              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1769160902/pebl/2034/logo_08.svg" alt="Linksys" width="120" height="40"/></li>
+              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1769160903/pebl/2034/logo_09.svg" alt="Glaukos" width="120" height="40"/></li>
+              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1769160903/pebl/2034/logo_10.svg" alt="Paige" width="120" height="40"/></li>
+            </ul>
+          </div>
+      </div>
+    </div>
+  `;
+
+document.querySelector('.node__content .layout:nth-child(2)').innerHTML = `
+    <div class="spz-hero-logos spz-hero-logos-black">
+      <div class="spz-hero-wrapper">
+        <strong>Trusted by global leaders</strong>
+	    	<div class="spz-logo-track">
+	            <ul class="spz-logo-list">
+	              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1770028424/pebl/2034/logo_01_black.svg" alt="LastPass" width="120" height="40"/></li>
+	              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1770028424/pebl/2034/logo_02_black.svg" alt="crunchbase" width="120" height="40"/></li>
+	              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1769096875/pebl/2034/logo_03_black.svg" alt="attentive" width="120" height="40"/></li>
+	              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1769096875/pebl/2034/logo_04_black.svg" alt="Materialize" width="120" height="40"/></li>
+	              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1769096875/pebl/2034/logo_05_black.svg" alt="consensys" width="120" height="40"/></li>
+	              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1770028424/pebl/2034/logo_06_black.svg" alt="Penguin Random House" width="120" height="40"/></li>
+	              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1769160902/pebl/2034/logo_07_black.svg" alt="anuvu" width="120" height="40"/></li>
+	              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1769160902/pebl/2034/logo_08_black.svg" alt="Linksys" width="120" height="40"/></li>
+	              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1769160903/pebl/2034/logo_09_black.svg" alt="Glaukos" width="120" height="40"/></li>
+	              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1769160903/pebl/2034/logo_10_black.svg" alt="Paige" width="120" height="40"/></li>
+	            </ul>
+	            <ul aria-hidden class="spz-logo-list">
+	              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1770028424/pebl/2034/logo_01_black.svg" alt="LastPass" width="120" height="40"/></li>
+	              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1770028424/pebl/2034/logo_02_black.svg" alt="crunchbase" width="120" height="40"/></li>
+	              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1769096875/pebl/2034/logo_03_black.svg" alt="attentive" width="120" height="40"/></li>
+	              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1769096875/pebl/2034/logo_04_black.svg" alt="Materialize" width="120" height="40"/></li>
+	              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1769096875/pebl/2034/logo_05_black.svg" alt="consensys" width="120" height="40"/></li>
+	              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1770028424/pebl/2034/logo_06_black.svg" alt="Penguin Random House" width="120" height="40"/></li>
+	              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1769160902/pebl/2034/logo_07_black.svg" alt="anuvu" width="120" height="40"/></li>
+	              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1769160902/pebl/2034/logo_08_black.svg" alt="Linksys" width="120" height="40"/></li>
+	              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1769160903/pebl/2034/logo_09_black.svg" alt="Glaukos" width="120" height="40"/></li>
+	              <li><img src="https://res.cloudinary.com/spiralyze/image/upload/v1769160903/pebl/2034/logo_10_black.svg" alt="Paige" width="120" height="40"/></li>
+	            </ul>
+	        </div>
+      </div>
+    </div>
+  `;
